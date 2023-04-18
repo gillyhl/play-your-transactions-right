bc10dbfaf1b0005ea548d10e5d465aefc185ff76
Gilbert Holland-Lloyd <gilbert.holland-lloyd@moneyhub.com>
2023-04-18 10:18:12 +0100

    Font change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter, Tilt_Prism } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const tilt = Tilt_Prism({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="container mx-auto">
       <div className="flex min-h-screen flex-col items-center justify-between lg:p-24 p-4">
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <div className="">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
             src="/moneyhub_icon_white.svg"
@@ -19,17 +20,17 @@ export default function Home() {
         </div>
 
         <div className="mb-8 grid text-center">
-          <h1 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+          <h1 className={`${tilt.className} mb-3 text-8xl font-semibold`}>
             Play Your Transactions Right
           </h1>
         </div>
         
         <div className="mb-8 grid text-center grid-cols-2 gap-8 w-full">
-          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          <button className="bg-green-900 hover:bg-green-700 text-white font-bold py-4 px-4 border-b-4 border-green-700 hover:border-green-500 rounded">
             Without Descriptions
           </button>
-          <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            With Descriptions
+          <button className="bg-green-900 hover:bg-green-700 text-white font-bold py-4 px-4 border-b-4 border-green-700 hover:border-green-500 rounded">
+            Without Descriptions
           </button>
         </div>
       </div>
